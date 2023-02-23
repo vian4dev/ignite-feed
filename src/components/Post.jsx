96073122a3262853ee861630785da040ef6c839b
Gabriel Viana <viannadoze@gmail.com>
2023-02-23 15:05:12 -0300

    inserting comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,9 +9,10 @@ import styles from './Post.module.css';
 
 export function Post( {author, publishedAt, content}) {
     const [comments, setComments] = useState([
-        1,
-        2,
+        'Post muito bacana, hein?!'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
     
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR,
@@ -25,9 +26,14 @@ export function Post( {author, publishedAt, content}) {
     function handleCreateNewComment() {
         event.preventDefault()
 
-
-        setComments([...comments, comments.length + 1])
+        setComments([...comments, newCommentText ]);
+        setNewCommentText('');
     }
+
+    function handleNewCommentChange() {
+        setNewCommentText(event.target.value)
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -58,8 +64,11 @@ export function Post( {author, publishedAt, content}) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea 
+                <textarea
+                  name="comment" 
                   placeholder='Deixe um comentário'
+                  value={newCommentText}
+                  onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -69,7 +78,7 @@ export function Post( {author, publishedAt, content}) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment />
+                    return <Comment content={comment}/>
                 })}
             </div>
         </article>
