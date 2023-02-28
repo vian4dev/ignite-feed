407d6961deeab9a00b595e959ada2d0fe26a845a
Gabriel Viana <viannadoze@gmail.com>
2023-02-27 22:06:34 -0300

    Update Ignite logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import styles from './Header.module.css'
 import IgniteLogo from '../assets/Ignite-logo.svg'
+import IgniteFeed from '../assets/Heading.svg'
 
 export function Header() {
     return (
         <header className={styles.header}>
             <img src={IgniteLogo} alt="Logotipo do Ignite"/>
+            <img src={IgniteFeed} alt="Ignite Feed"/>
         </header>
     )
 }
